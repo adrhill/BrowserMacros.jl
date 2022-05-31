6ce1dce208d820909c852af0591e127d3cd36022
Documenter.jl <documenter@juliadocs.github.io>
2022-05-31 22:55:26 +0000

    build based on 81811e2

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = BrowserMacros","category":"page"},{"location":"#BrowserMacros","page":"Home","title":"BrowserMacros","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for BrowserMacros.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [BrowserMacros]","category":"page"}]
+}

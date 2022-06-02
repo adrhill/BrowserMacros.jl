fd340cb8959421c34668fc8b5f9836127f52bed2
Adrian Hill <adrian.hill@mailbox.org>
2022-06-02 20:42:46 +0200

    Literate package imports

--- src/BrowserMacros.jl
+++ src/BrowserMacros.jl
@@ -1,15 +1,23 @@
 module BrowserMacros
 
+# The package that makes all of this possible:
+using DefaultApplication
+
+# Escape special characters in URLs:
+using URIs: escapeuri
+
+# Imports used by `@wwwhich` to look up Repository URLs in Registry:
 using Base: UUID
 using Pkg.Types: EnvCache
 using Pkg.Registry: PkgInfo, RegistryInstance, reachable_registries
-using DefaultApplication
-using URIs: escapeuri
+
+# Generate macro `@wwwhich` from function `wwwhich` the same way `@which` is generated:
+using InteractiveUtils: gen_call_with_extracted_types
 
 open_browser(url) = DefaultApplication.open(url), return nothing
 
-include("wwwhich.jl")
 include("search.jl")
+include("wwwhich.jl")
 
 export google, @google, ddg, @ddg
 end # module

37d0db2af320bfc6fc81e9b867adc3be587cae1b
Adrian Hill <adrian.hill@mailbox.org>
2022-10-06 01:37:08 +0200

    Update tests

--- test/test_urls.jl
+++ test/test_urls.jl
@@ -1,26 +1,30 @@
-using BrowserMacros: get_method_url, get_google_url, get_ddg_url
+using BrowserMacros: MethodInfo, method_url, google_url, ddg_url
 
 # Load some modules to test wwwhich's URLs:
 using Base: UUID # Julia Base
 using InteractiveUtils
 using DefaultApplication # GitHub
 
-@test @inferred get_google_url("123!@#\$%^&*()_+QWERT{}|") ==
+@test @inferred google_url("123!@#\$%^&*()_+QWERT{}|") ==
     "https://www.google.com/search?q=123%21%40%23%24%25%5E%26%2A%28%29_%2BQWERT%7B%7D%7C"
-@test @inferred  get_ddg_url("줄리아, جوليا") ==
+@test @inferred ddg_url("줄리아, جوليا") ==
     "https://duckduckgo.com/?q=%EC%A4%84%EB%A6%AC%EC%95%84%2C%20%D8%AC%D9%88%D9%84%D9%8A%D8%A7"
 
 # Base
 m = @which sqrt(1.0)
-url = @inferred get_method_url(m)
+info = @inferred MethodInfo(m)
+url = @inferred method_url(info)
 @test !isnothing(url)
 
 # stdlib
 m = @which @which sqrt(1.0)
-url = @inferred get_method_url(m)
+info = @inferred MethodInfo(m)
+url = @inferred method_url(info)
 @test !isnothing(url)
 
 # External packages
 m = Base.which(DefaultApplication.open, (String,))
-url = @inferred get_method_url(m)
-@test !isnothing(url)
+info = @inferred MethodInfo(m)
+url = @inferred method_url(info)
+@test url ==
+    "https://github.com/tpapp/DefaultApplication.jl/blob/v1.1.0/src/DefaultApplication.jl#L18"

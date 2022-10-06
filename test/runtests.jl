fdce6a73e0d974dfd5cbae22ba582b66f21b785b
Adrian Hill <adrian.hill@mailbox.org>
2022-10-06 02:22:40 +0200

    Fix formatting of testsets

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,7 @@ using BrowserMacros
 using Test
 
 @testset "BrowserMacros.jl" begin
-    @testset "Test URLs" include("test_urls.jl")
+    @testset "Test URLs" begin
+        include("test_urls.jl")
+    end
 end

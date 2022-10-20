6027ed922145c82313fa4a378d4af38e6749bb73
Adrian Hill <adrian.hill@mailbox.org>
2022-10-20 19:30:51 +0200

    Return URLs when calling macros

--- src/utils.jl
+++ src/utils.jl
@@ -1,4 +1,7 @@
-open_browser(url) = DefaultApplication.open(url), return nothing
+function open_browser(url)
+    DefaultApplication.open(url)
+    return url
+end
 
 captures(r::Regex, s) = match(r, s).captures
 ismatching(r::Regex, s) = !isnothing(match(r, s))

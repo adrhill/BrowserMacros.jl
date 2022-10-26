0b7dd76d5d6474cdd43b100278b50d3f0662254a
Adrian Hill <adrian.hill@mailbox.org>
2022-10-27 00:33:28 +0200

    Generate search engine docstrings

--- src/search.jl
+++ src/search.jl
@@ -1,6 +1,7 @@
-const SEARCH_ENGINES = Dict(
+const SEARCH_ENGINES = (
     :google => "https://www.google.com/search?q=", :ddg => "https://duckduckgo.com/?q="
 )
+
 for (fname, url) in SEARCH_ENGINES
     @eval begin
         function ($fname)(query::AbstractString; open_browser=true)
@@ -11,7 +12,7 @@ for (fname, url) in SEARCH_ENGINES
     end
 end
 
-for fname in keys(SEARCH_ENGINES)
+for fname in map(first, SEARCH_ENGINES)
     @eval begin
         macro ($fname)(ex0...)
             # kwarg parsing adapted from InteractiveUtils' gen_call_with_extracted_types_and_kwargs
@@ -33,80 +34,38 @@ for fname in keys(SEARCH_ENGINES)
     end
 end
 
-"""
-    google(query::String)
+# Docstrings
+for fname in map(first, SEARCH_ENGINES)
+    name = string(fname)
+    @eval begin
+        @doc """
+            $($name)(query::String)
 
-Open a tab with the specified Google search query in the default browser.
-Also usable as the macro [`@google`](@ref).
+        Open a tab with the specified search query in the default browser.
+        Also usable as the macro [`@$($name)`](@ref).
 
-## Example
-```julia-repl
-julia> google("Why is julialang called Julia?")
-```
-```julia-repl
-julia> url = google("Why is julialang called Julia?"; open_browser=false)
-```
+        ## Examples
+        ```julia
+        $($name)("Why is julialang called Julia?") # opens default browser
 
-See also: [`ddg`](@ref).
-"""
-google
+        url = $($name)("Why is julialang called Julia?"; open_browser=false)
+        ```
+        """ $fname
+    end
 
-"""
-    @google query
+    @eval begin
+        @doc """
+            @$($name) query
 
-Open a tab with the specified Google search query in the default browser.
-Also usable as the function [`google`](@ref).
+        Open a tab with the specified search query in the default browser.
+        Also usable as the function [`$($name)`](@ref).
 
-## Examples
-```julia-repl
-julia> @google "Why is julialang called Julia?"
-```
-```julia-repl
-julia> url = @google open_browser=false "Why is julialang called Julia?"
-```
+        ## Examples
+        ```julia
+        @$($name) "Why is julialang called Julia?" # opens default browser
 
-See also: [`@ddg`](@ref).
-"""
-:@google
-
-"""
-    ddg(query::String)
-
-Open a tab with the specified DuckDuckGo search query in the default browser.
-Also usable as the macro [`@ddg`](@ref).
-
-## Examples
-```julia-repl
-julia> ddg("Why is julialang called Julia?")
-```
-```julia-repl
-julia> ddg("!gi Julia Logo")
-```
-```julia-repl
-julia> url = ddg("!gi Julia Logo"; open_browser=false)
-```
-
-See also: [`google`](@ref).
-"""
-ddg
-
-"""
-    @ddg query
-
-Open a tab with the specified Google search query in the default browser.
-Also usable as the function [`ddg`](@ref).
-
-## Examples
-```julia-repl
-julia> @ddg "Why is julialang called Julia?"
-```
-```julia-repl
-julia> @ddg "!gi Julia Logo"
-```
-```julia-repl
-julia> url = @ddg open_browser=false "!gi Julia Logo"
-```
-
-See also: [`@google`](@ref).
-"""
-:@ddg
+        url = @$($name) open_browser=false "Why is julialang called Julia?"
+        ```
+        """ $(Symbol("@$name"))
+    end
+end
